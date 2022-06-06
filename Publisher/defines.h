286a3d29a9a22f605be680de4319e7632547acbb
Yuriy Lazutin <yuriylazutin@gmail.com>
2022-06-06 23:45:01 +0700

    Actualized defines.h Improved Makefile

--- Publisher/defines.h
+++ Publisher/defines.h
@@ -1,11 +1,15 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define   SERVER_PORT             5810
-#define   SERVER_LISTEN_BACKLOG   32
-#define   ID_SIZE                 4
-#define   FLAGS_SIZE              4
-#define   SIG_SIZE                32
+#define   SERVER_PORT                     5810
+#define   SERVER_LISTEN_BACKLOG           32
+#define   ID_SIZE                         4
+#define   FLAGS_SIZE                      4
+#define   SIG_SIZE                        32
+#define   TOTAL_REQ_TIME_LIMIT            30000 // 30 sec
+#define   READ_BLOCK_TIME_LIMIT           5000 // 5 sec
+#define   WRITE_BLOCK_TIME_LIMIT          5000 // 5 sec
+#define   MAX_CLOSE_DESCRIPTOR_ATTEMPTS   5
 
 #define   TINY_BUFFER_SIZE        8
 #define   SMALL_BUFFER_SIZE       256
@@ -17,9 +21,16 @@
 #define   BAD_METHOD             2
 #define   NOT_FOUND              3
 #define   CONNECTION_CLOSED      4
-#define   MALLOC_FILED           5
+#define   MALLOC_FAILED          5
 #define   PATH_OVERFLOW          6
-#define   OPEN_FILE_ERROR        7
-#define   READ_FILE_ERROR        8
+#define   PATH_INVALID           7
+#define   OPEN_FILE_ERROR        8
+#define   READ_FILE_ERROR        9
+#define   READ_LINK_ERROR       10
+#define   TIME_OUT              11
+#define   POLL_ERROR            12
+#define   READ_BLOCK_ERROR      13
+#define   WRITE_BLOCK_ERROR     14
+#define   UNKNOWN_ERROR         15
 
 #endif // DEFINES_H
