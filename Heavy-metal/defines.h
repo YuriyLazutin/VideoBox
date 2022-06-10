b94bb792810b1da0a961347a4564c05c8b8bc113
Yuriy Lazutin <yuriylazutin@gmail.com>
2022-06-10 21:19:04 +0700

    Heavy-metal: actualized defines.h

--- Heavy-metal/defines.h
+++ Heavy-metal/defines.h
@@ -10,6 +10,8 @@
 #define   READ_BLOCK_TIME_LIMIT           5000 // 5 sec
 #define   WRITE_BLOCK_TIME_LIMIT          5000 // 5 sec
 #define   MAX_CLOSE_DESCRIPTOR_ATTEMPTS   5
+#define   MAX_CREATE_ID_DIR_ATTEMPTS      1000
+#define   ID_CHARS                        "0123456789abcdefghijklmnopqrstuvwxy"
 
 #define   TINY_BUFFER_SIZE        8
 #define   SMALL_BUFFER_SIZE       256
@@ -31,6 +33,16 @@
 #define   POLL_ERROR            12
 #define   READ_BLOCK_ERROR      13
 #define   WRITE_BLOCK_ERROR     14
-#define   UNKNOWN_ERROR         15
+#define   OPEN_DIR_ERROR        15
+#define   STAT_FAILED           16
+#define   PIPE_FAILED           17
+#define   FORK_FAILED           18
+#define   DUP2_FAILED           19
+#define   EXEC_FAILED           20
+#define   WAITPID_FAILED        21
+#define   DUPLICATE_FOUND       22
+#define   NOT_FILE              23
+#define   RENAME_FILE_FAILED    24
+#define   UNKNOWN_ERROR       1000
 
 #endif // DEFINES_H
